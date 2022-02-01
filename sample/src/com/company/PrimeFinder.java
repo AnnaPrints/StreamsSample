c2a71dbba357c798761acb6c520cc90336413696
AnnaPrints <65970510+AnnaPrints@users.noreply.github.com>
2022-02-01 10:09:57 +0300

    Add files via upload
    hj

---
+++ sample/src/com/company/PrimeFinder.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.stream.IntStream;
+
+public class PrimeFinder {
+    public static void main(String[] args) {
+//        IntStream.iterate(1, n -> n + 1)
+//                .skip(Integer.parseInt(args[0]))
+//                .limit(Integer.parseInt(args[1]))
+//                .filter(PrimeTest::isPrime)
+//                .forEach(System.out::println);
+    }
+}
